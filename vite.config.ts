f4c74230564daef0f697429870384ad31ebf37da
Raqibj021 <raqibj021@gmail.com>
2026-07-31 00:57:12 +0500

    Configure production launch: vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: process.env.GITHUB_ACTIONS ? "/card-tj2/" : "/",
+  base: process.env.VITE_BASE_PATH || (process.env.GITHUB_ACTIONS ? "/card-tj2/" : "/"),
   plugins: [react(), tailwindcss()],
   build: {
     sourcemap: false,
